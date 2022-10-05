fc5a96a86ec837ddc25754399591aebc481c67ee
scusate <nevadialonso@gmail.com>
2022-10-06 01:35:34 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let count = 0;
+
+document.querySelector("#increase").addEventListener("click", () => {
+  changeCount(1);
+});
+
+document.querySelector("#decrease").addEventListener("click", () => {
+  changeCount(-1);
+});
+
+changeCount = (number) => {
+  count += number;
+  document.querySelector("#count").textContent = count;
+}
